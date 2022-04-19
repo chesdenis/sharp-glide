2f32dcb84f017e317f99b75c35a625521823ee0f
chesdenis <chesdenis@gmail.com>
2022-04-20 00:48:06 +0300

    benchmark project added

---
+++ benchmark/SharpGlide.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using SharpGlide.Benchmark.Tunnels;
+
+namespace SharpGlide.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Md5VsSha256>();
+        }
+    }
+}
