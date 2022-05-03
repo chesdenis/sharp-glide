568d5a3be0526baa04b7e96c76cf331a131b7c6e
chesdenis <chesdenis@gmail.com>
2022-05-03 14:17:31 +0300

    disabled certificate check and https

--- examples/SharpGlide.WebApps.YandexDiskUploader/Program.cs
+++ examples/SharpGlide.WebApps.YandexDiskUploader/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -32,16 +33,13 @@ namespace SharpGlide.WebApps.YandexDiskUploader
                 .ConfigureHostConfiguration(x=>x.AddConfiguration(Configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    var certPath = Configuration.GetValue<string>("Https:CertPath");
-                    var certPassword = Configuration.GetValue<string>("Https:CertPassword");
                     webBuilder.UseKestrel(options =>
                     {
-                        options.Listen(IPAddress.Any, 5050,
-                            listenOptions =>
-                            {
-                                listenOptions.UseHttps(certPath,
-                                    certPassword);
-                            });
+                        options.ConfigureHttpsDefaults(options =>
+                            options.ClientCertificateMode = ClientCertificateMode.NoCertificate);
+                        
+                        options.Listen(IPAddress.Any, 5050);
+                        
                     });
                     webBuilder.UseStartup<Startup>();
                 });
